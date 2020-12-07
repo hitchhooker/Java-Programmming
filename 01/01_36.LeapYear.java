14492f5d5f442a18a7ef2502a36d1bfe73c4a15e
hitchhooker <tommi@niemi.lol>
2020-12-07 04:13:44 +0000

    finish 01 and start 02

---
+++ 01/01_36.LeapYear.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // A year is a leap year if it is divisible by 4. However, if the year is divisible by 100, 
+        // then it is a leap year only when it is also divisible by 400.
+        // Write a program that reads a year from the user, and checks whether or not it is a leap year.
+        System.out.println("Give a year:");
+        int year = Integer.valueOf(scan.nextLine());
+        if (year % 4 != 0) {
+            System.out.println("This is not a leap year.");
+        } else if (year % 100 == 0 && year % 400 != 0) {
+            System.out.println("This is not a leap year.");
+        } else {
+            System.out.println("This is a leap year.");
+        }
+
+    }
+}
+
